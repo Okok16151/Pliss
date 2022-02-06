539e7300c327854700298ebbeff9321d1ef6cf05
Okok16151 <97395275+Okok16151@users.noreply.github.com>
2022-02-06 13:32:43 +0800

    Delete assets/preload/data/red directory

--- assets/preload/data/red/script.lua
+++
@@ -1,6 +0,0 @@
-function onCreate()
-	setPropertyFromClass('GameOverSubstate', 'characterName', 'flake'); --Character json file for the death animation
-	setPropertyFromClass('GameOverSubstate', 'deathSoundName', 'flaky die'); --put in mods/sounds/
-	setPropertyFromClass('GameOverSubstate', 'loopSoundName', 'game over'); --put in mods/music/
-	setPropertyFromClass('GameOverSubstate', 'endSoundName', 'retry'); --put in mods/music/
-end
