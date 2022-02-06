e41d97abf305ece1bd07f302af526c2a2bafa6ba
Okok16151 <97395275+Okok16151@users.noreply.github.com>
2022-02-06 13:37:27 +0800

    Add files via upload

---
+++ assets/preload/data/turvy/script.lua
@@ -0,0 +1,4 @@
+function onCreate()
+	setPropertyFromClass('GameOverSubstate', 'loopSoundName', 'game over'); --put in mods/music/
+	setPropertyFromClass('GameOverSubstate', 'endSoundName', 'retry'); --put in mods/music/
+end
